d934f01d39c6040dce8d32acc66ab18e48078f73
Sette <brunosilvasette@gmail.com>
2018-08-28 21:19:59 -0400

    Preprocess

--- TideneReadCorpus.py
+++ TideneReadCorpus.py
@@ -25,31 +25,29 @@ class TideneIterCSVW2V(object):
 
 
 class TideneIterCSVCorpus(object):
-	def __init__(self,csvfiles):
+	def __init__(self,csvfile):
 		self.stopwords = set(stopwords.words('english'))
 
 		self.tokenizer = nltk.tokenize.RegexpTokenizer(r'\w+')
 
 		
-		for csvfile in csvfiles:
-			csv.field_size_limit(10**9)
-			self.reader = csv.reader(open(csvfile,"r"),delimiter=",", quoting=csv.QUOTE_MINIMAL)
-			self.reader.__next__()
+		csv.field_size_limit(10**9)
+		self.reader = csv.reader(open(csvfile,"r"),delimiter=",", quoting=csv.QUOTE_MINIMAL)
+		self.reader.__next__()
 
-			apaga = csv.reader(open(csvfile,"r"),delimiter=",", quoting=csv.QUOTE_MINIMAL)
-			apaga.__next__()
-			self.totalsents = (len(list(apaga)))
+		apaga = csv.reader(open(csvfile,"r"),delimiter=",", quoting=csv.QUOTE_MINIMAL)
+		apaga.__next__()
+		self.totalsents = (len(list(apaga)))
 
 	def __iter__(self):
-		index = 1
+		index = 0
 		for index,row in enumerate(self.reader):
 			print("Progress:", (index+1), "/", self.totalsents)
-			review_text = re.sub("[^a-zA-Z]", " ", row[6].lower())
-			review_text = [w for w in self.tokenizer.tokenize(review_text) if w not in self.stopwords]
-			row[6] = str1 = ' '.join(review_text)
+			row[6] = re.sub("[^a-zA-Z]", " ", row[6].lower())
+			row[6] = [w for w in self.tokenizer.tokenize(row[6]) if w not in self.stopwords and len(w) > 3]
+			row[6] = ' '.join(row[6])
 			index += 1
 
-			
-
+		
 			yield row #['data']
 
